1cd91ff050dfe4228653ee45773cd165d882fe14
priyankadasarigt <priyankadasari1@hotmail.com>
2025-11-23 16:11:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-export const metadata = { title: "Join @HuntTV" };
+export const metadata = { title: "HuntTV" };
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Ex */}
-        <script src="/m.js"></script>
-        {/* Ext */}
-        <script src="/obf.js"></script>
+        {/* External CSS (hidden content, not inline in view-source) */}
+        <link rel="stylesheet" href="/styles.css" />
+        {/* External DevTool blocker script */}
+        <script src="/m.js" defer></script>
       </head>
       <body>
         {children}
