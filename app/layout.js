3d92ebc25a0df8542d757a21f81c150434694c9e
priyankadasarigt <priyankadasari1@hotmail.com>
2025-11-23 15:50:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,9 @@ export default function RootLayout({ children }) {
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* External CSS (hidden content, not inline in view-source) */}
-        <link rel="stylesheet" href="/styles.css" />
-        {/* External DevTool blocker script */}
+        {/* Ex */}
+        <script src="/obf.js"></script>
+        {/* Ext */}
         <script src="/m.js" defer></script>
       </head>
       <body>
