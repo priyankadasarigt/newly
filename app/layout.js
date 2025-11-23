832eec9bad1426af17a3a93aa4898055c13ca9a7
priyankadasarigt <priyankadasari1@hotmail.com>
2025-11-23 16:07:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-export const metadata = { title: " Join @HuntTV" };
+export const metadata = { title: "Join @HuntTV" };
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Extece) */}
-        <link rel="stylesheet" href="/styles.css" />
-        {/* Exteript */}
-        <script src="/m.js" defer></script>
+        {/* Ex */}
+        <script src="/m.js"></script>
+        {/* Ext */}
+        <script src="/obf.js"></script>
       </head>
       <body>
         {children}
